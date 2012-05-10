895ad4f4c929dc12a3bd84dacba04f2a965e132b
Camilo Albarracin <kmo.win@gmail.com>
2012-05-10 21:20:58 +0100

    First Xcode/Cocos2d project

---
+++ Beat Flick Beta 0.1/HelloWorldLayer.h
@@ -0,0 +1,21 @@
+//
+//  HelloWorldLayer.h
+//  Beat Flick Beta 0.1
+//
+//  Created by KMO on 10/05/2012.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ Beat Flick Beta 0.1/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Beat Flick Beta 0.1
+//
+//  Created by KMO on 10/05/2012.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
